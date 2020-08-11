ca197e6f9c0e5a11c2da88b3fdd7c299898cd28b
Shari Reyes <shari2289@yahoo.com>
2020-08-11 13:38:02 -0500

    adding some basic framing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-console.log("this works");
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
