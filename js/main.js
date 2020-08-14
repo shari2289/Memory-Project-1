93281659f76f04966ec2724c1a505c5ceeef7ab2
Shari Reyes <shari2289@yahoo.com>
2020-08-14 05:35:22 -0500

    adding win/loss logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,39 @@
 const cards = document.querySelectorAll(".card");
-const timer = document.getElementById("time-left");
 
 let lockGrid = false;
 let firstCard, secondCard;
 let flippedCard = false;
+let timeLeft = 90;
+let matchingCards = [];
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
+
+function startTimer(duration, display) {
+  var timer = duration,
+    minutes,
+    seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 1 ? "0" + minutes : minutes;
+    seconds = seconds < 0 ? "30" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+
+window.onload = function () {
+  var gameTime = 90 * 1,
+    display = document.querySelector("#time-left");
+  startTimer(gameTime, display);
+  if ("#time-left" === 0);
+  gameOver();
+};
 
 function flipCard() {
   if (lockGrid) return;
@@ -26,17 +54,17 @@ function flipCard() {
 function checkIfMatched() {
   let isAMatch = firstCard.dataset.style === secondCard.dataset.style;
 
-  isAMatch ? lockCard() : unflipCard();
+  isAMatch ? itsAMatch() : notAMatch();
 }
 
-function lockCard() {
+function itsAMatch() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
-
+  matchingCards.push(firstCard, secondCard);
   refreshGame();
 }
 
-function unflipCard() {
+function notAMatch() {
   lockGrid = true;
 
   setTimeout(() => {
@@ -44,12 +72,20 @@ function unflipCard() {
     secondCard.classList.remove("flip");
 
     refreshGame();
-  }, 900);
+  }, 1000);
 }
 
 function refreshGame() {
   [flippedCard, lockGrid] = [false, false];
   [firstCard, secondCard] = [null, null];
+}
+
+function gameOver() {
+  if (timeLeft === 0) display = document.querySelector("#game-over");
+}
+
+function gameWinner() {
+  if (matchingCards.length !== 12) display = document.querySelector("winner");
 }
 
 (function shuffle() {
