88568295e9124a5c7cf0861dd4539ad39d0ad8ec
Shari Reyes <shari2289@yahoo.com>
2020-08-13 22:54:30 -0500

    card flip

--- js/main.js
+++ js/main.js
@@ -1,23 +1,17 @@
-/*----- constants -----*/
 const cards = document.querySelectorAll(".card");
 
 let flippedCard = false;
 let firstCard, secondCard;
 
-/*----- app's state (variables) -----*/
-
-/*----- cached element references -----*/
-/*----- event listeners -----*/
 cards.forEach((card) => card.addEventListener("click", flipCard));
-/*----- functions -----*/
+
 function flipCard() {
   this.classList.add("flip");
 
-  if (flippedCard) {
+  if (!flippedCard) {
     flippedCard = true;
     firstCard = this;
-  } else {
-    flippedCard = false;
-    secondCard = this;
+
+    console.log(flippedCard, firstCard);
   }
 }
