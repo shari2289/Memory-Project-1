c55ba9cec5c24e36bc61156ac63281d23e105001
Shari Reyes <shari2289@yahoo.com>
2020-08-14 02:01:41 -0500

    adding shuffle

--- js/main.js
+++ js/main.js
@@ -9,30 +9,52 @@ cards.forEach((card) => card.addEventListener("click", flipCard));
 
 function flipCard() {
   if (lockGrid) return;
+  if (this === firstCard) return;
 
   this.classList.add("flip");
 
   if (!flippedCard) {
     flippedCard = true;
     firstCard = this;
-  } else {
-    flippedCard = false;
-    secondCard = this;
-
-    checkIfMatched();
+    return;
   }
 
-  function checkIfMatched() {
-    if (firstCard.dataset.style === secondCard.dataset.style) {
-      firstCard.removeEventListener("click", flipCard);
-      secondCard.removeEventListener("click", flipCard);
-    } else {
-      lockGrid = true;
-      setTimeout(() => {
-        firstCard.classList.remove("flip");
-        secondCard.classList.remove("flip");
-        lockGrid = false;
-      }, 900);
-    }
-  }
+  secondCard = this;
+  checkIfMatched();
 }
+
+function checkIfMatched() {
+  let isAMatch = firstCard.dataset.style === secondCard.dataset.style;
+
+  isAMatch ? lockCard() : unflipCard();
+}
+
+function lockCard() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  refreshGame();
+}
+
+function unflipCard() {
+  lockGrid = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    refreshGame();
+  }, 900);
+}
+
+function refreshGame() {
+  [flippedCard, lockGrid] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
