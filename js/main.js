18ea0a5e495d8a4dd7583ea2d517c83e9688a6c9
Shari Reyes <shari2289@yahoo.com>
2020-08-14 01:06:06 -0500

    adding card lock

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 const cards = document.querySelectorAll(".card");
 const timer = document.getElementById("time-left");
 
-let matchedCards = [];
+let lockGrid = false;
 let firstCard, secondCard;
 let flippedCard = false;
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
 function flipCard() {
+  if (lockGrid) return;
+
   this.classList.add("flip");
 
   if (!flippedCard) {
@@ -17,14 +19,20 @@ function flipCard() {
     flippedCard = false;
     secondCard = this;
 
+    checkIfMatched();
+  }
+
+  function checkIfMatched() {
     if (firstCard.dataset.style === secondCard.dataset.style) {
       firstCard.removeEventListener("click", flipCard);
       secondCard.removeEventListener("click", flipCard);
     } else {
+      lockGrid = true;
       setTimeout(() => {
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
-      }, 1000);
+        lockGrid = false;
+      }, 900);
     }
   }
 }
